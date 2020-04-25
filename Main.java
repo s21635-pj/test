68df890d3c2d91cd1cf3d10e41b4541da9cfbfaf
Paulina Klebba <s21635@pjwstk.edu.pl>
2020-04-25 11:28:05 +0200

    coś

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+         zmienne zmienne = new zmienne(2,3);
+        System.out.println(zmienne.pole());
+        System.out.println("zmiana");
+    }
+}
