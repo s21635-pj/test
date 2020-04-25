bcf9c53adfaed56788292dcf1806547408ed866a
Paulina Klebba <s21635@pjwstk.edu.pl>
2020-04-25 11:43:36 +0200

    zmiana 2

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main {
          zmienne zmienne = new zmienne(2,3);
         System.out.println(zmienne.pole());
         System.out.println("zmiana");
+        System.out.println("zmiana2");
     }
 }
